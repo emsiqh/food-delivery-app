34ef8c99a2a51a6d6e1c955086fcb58e453b3afe
emsiqh <emsiqh2k1@gmail.com>
2023-02-15 15:25:06 +0700

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+export const headerLinks = [
+    {
+        name: "Home",
+        link: "/home",
+    },
+    {
+        name: "Menu",
+        link: "/menu",
+    },
+    {
+        name: "About Us",
+        link: "/about",
+    },
+    {
+        name: "Service",
+        link: "/service",
+    },
+]
